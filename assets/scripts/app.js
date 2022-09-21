a4721fdd9eb809faae1e77e8524f009bd431cd8a
Msafiri Juma Esmail <msafirijesmail@gmail.com>
2022-09-21 16:59:16 +0300

    working on user selection expense or revenue section

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,9 +1,21 @@
 const header = document.querySelector(".header");
+const userTypeSelection = document.querySelector("#userTypeSelection");
 
 window.addEventListener("scroll", () => {
     if (window.document.documentElement.scrollTop >= 60) {
         header.classList.add("navbar-scroll")
     } else {
         header.classList.remove("navbar-scroll")
+    }
+})
+
+userTypeSelection.addEventListener("change", (e) => {
+    userInput = e.target.value;
+    if (userInput === "-1") {
+        window.location.href = "../../homepage.html"
+    } else if (userInput === "expense") {
+        window.location.href = "../../expenditures.html"
+    } else {
+        window.location.href = "../../revenues.html"
     }
 })
