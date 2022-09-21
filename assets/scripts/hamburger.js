28eaebc16b0f0ae7cb1ce071ee01761dd26663e7
Msafiri Juma Esmail <msafirijesmail@gmail.com>
2022-09-22 02:54:08 +0300

    working on theme mode and fix hamburger on click event

--- assets/scripts/hamburger.js
+++ assets/scripts/hamburger.js
@@ -1,12 +1,16 @@
 const hamburger = document.querySelector(".hamburger");
-const navbarMenu = document.querySelector(".nav-menu")
+const navbarMenu = document.querySelector(".nav-menu");
+const textMenu = document.querySelector(".text-menu")
+const hamburgerIcon = document.querySelector("#hamburgerIcon");
 
 hamburger.addEventListener("click", () => {
     if (!(navbarMenu.classList.contains("active"))) {
         navbarMenu.classList.add("active");
-        hamburger.innerHTML = "x"
+        hamburgerIcon.classList.replace("fa-hamburger", "fa-times");
+        textMenu.innerHTML = "Close";
     } else {
         navbarMenu.classList.remove("active");
-        hamburger.innerHTML = "= Menu"
+        hamburgerIcon.classList.replace("fa-times", "fa-hamburger");
+        textMenu.innerHTML = "Menu";
     }
 })
--- assets/scripts/theme.js
+++ assets/scripts/theme.js
@@ -1,12 +1,14 @@
 const themeToggler = document.querySelector("#themeToggler");
-
+const themeIcon = document.querySelector("#themeIcon");
 let darkMode = localStorage.getItem("dark-mode");
 
 const toggleDarkMode = () => {
+    themeIcon.classList.replace("fa-moon", "fa-sun");
     localStorage.setItem("dark-mode", "enabled");
 }
 
 const toggleLightMode = () => {
+    themeIcon.classList.replace("fa-sun", "fa-moon");
     localStorage.setItem("dark-mode", "disabled");
 }
 
@@ -18,9 +20,9 @@ if (darkMode == "enabled") {
 themeToggler.addEventListener("click", () => {
     darkMode = localStorage.getItem("dark-mode");
     if (darkMode === "disabled") {
-        toggleDarkMode()
+        toggleDarkMode();
     } else {
-        toggleLightMode()
+        toggleLightMode();
     }
 })
 
