88846743764989782cb96355554d92573f8608aa
Msafiri Juma Esmail <msafirijesmail@gmail.com>
2022-09-22 18:55:16 +0300

    add classes for dark mode for both header and footer

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -9,4 +9,3 @@ window.addEventListener("scroll", () => {
     }
 })
 
-
--- assets/scripts/theme.js
+++ assets/scripts/theme.js
@@ -1,13 +1,43 @@
 const themeToggler = document.querySelector("#themeToggler");
 const themeIcon = document.querySelector("#themeIcon");
+const footer = document.querySelector(".footer");
+const header = document.querySelector(".header");
 let darkMode = localStorage.getItem("dark-mode");
 
+const headerDarkMode = () => {
+    if (header.classList.contains("header")) {
+        header.classList.add("header-dark")
+    }
+}
+
+const footerDarkMode = () => {
+    if (footer.classList.contains("footer")) {
+        footer.classList.add("footer-dark")
+    }
+}
+
+const headerLightMode = () => {
+    if (header.classList.contains("header-dark")) {
+        header.classList.remove("header-dark")
+    }
+}
+
+const footerLightMode = () => {
+    if (footer.classList.contains("footer-dark")) {
+        footer.classList.remove("footer-dark")
+    }
+}
+
 const toggleDarkMode = () => {
+    headerDarkMode();
+    footerDarkMode();
     themeIcon.classList.replace("fa-moon", "fa-sun");
     localStorage.setItem("dark-mode", "enabled");
 }
 
 const toggleLightMode = () => {
+    headerLightMode()
+    footerLightMode();
     themeIcon.classList.replace("fa-sun", "fa-moon");
     localStorage.setItem("dark-mode", "disabled");
 }
