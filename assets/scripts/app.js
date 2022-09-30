850a8f3b8b7f60e8eb97876f90e90a1eb032ed81
Msafiri Juma Esmail <msafirijesmail@gmail.com>
2022-09-30 17:27:30 +0300

    fix footer position and fix background image

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,7 +2,7 @@ const header = document.querySelector(".header");
 const userTypeSelection = document.querySelector("#userTypeSelection")
 
 window.addEventListener("scroll", () => {
-    if (window.document.documentElement.scrollTop >= 60) {
+    if (window.document.documentElement.scrollTop >= 50) {
         header.classList.add("navbar-scroll")
     } else {
         header.classList.remove("navbar-scroll")
